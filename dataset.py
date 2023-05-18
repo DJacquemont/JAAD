ae11f1c1b12cf76b0fedcfc3b2dd497653f0ea20
Dimitri <dimitri.jacquemont@epfl.ch>
2023-05-18 13:38:54 +0200

    Cleaning WS, automated paths implemented

--- dataset.py
+++ dataset.py
@@ -1,38 +1,49 @@
+"""
+MIT License
+
+Copyright (c) 2018 I. Kotseruba
+Copyright (c) 2023 D. Jacquemont, K. Hinard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import pickle
 import numpy as np
 import xml.etree.ElementTree as ET
-from os.path import join, abspath, exists
-from os import listdir, makedirs
+from os.path import join, abspath, exists, dirname
+from os import listdir
 
 class JAAD(object):
-    def __init__(self, data_path='', regen_pkl=False):
+    def __init__(self, data_path=''):
         """
         Constructor of the jaad class
         :param data_path: Path to the folder of the dataset
-        :param regen_pkl: Whether to regenerate the database
         """
         self._year = '2016'
         self._name = 'JAAD'
-        self._regen_pkl = regen_pkl
         self._image_ext = '.png'
 
         # Paths
         self._jaad_path = data_path if data_path else self._get_default_path()
         assert exists(self._jaad_path), \
             'Jaad path does not exist: {}'.format(self._jaad_path)
-        self._annotation_path = join(self._jaad_path, 'annotations')
-
-    # Path generators
-    @property
-    def cache_path(self):
-        """
-        Generate a path to save cache files
-        :return: Cache file folder path
-        """
-        cache_path = abspath(self._jaad_path)
-        if not exists(cache_path):
-            makedirs(cache_path)
-        return cache_path
+        self._annotation_path = join(self._jaad_path, 'JAAD_DS/annotations')
 
     def _get_default_path(self):
         """
@@ -134,32 +145,18 @@ class JAAD(object):
                         annotations[ped_annt][new_id] = []
                         nbr_seq += int(((len(tmp_bbox) - 30 - 1)/30))
                         for i in range(0, int(((len(tmp_bbox) - 30 - 1)/30))):
-                            print("-----------------------------")
-                            print("pedestrian id                : " + str(new_id))
-                            print("Current sequence             : " + str(i))
                             
                             annotation_bbox = np.array(tmp_bbox[i*30:(i+2)*30])
                             annotation_occ = tmp_occ[i*30:(i+2)*30]
                             annotation_frames = tmp_frames[i*30:(i+2)*30]
 
                             end_idx_cross = min(len(tmp_cross[(i+2)*30:]), label_frames)
-
-                            print("Min between                  : " + str(len(tmp_cross[(i+2)*30:])) + " and " + str(label_frames) + " is " + str(end_idx_cross))
-
                             annotation_cross = np.amax(np.array(tmp_cross[(i+2)*30:(i+2)*30+end_idx_cross]))
 
                             annotations_dict = {'old_id': old_id, 'frames': annotation_frames,
                                                 'bbox': annotation_bbox, 'occlusion': annotation_occ, 'cross': annotation_cross}
 
                             annotations[ped_annt][new_id].append(annotations_dict)
-
-                            print("Current frames annotations   : " + str(i*30) + " - " + str((i+2)*30))
-                            print("Current frames label         : " + str((i+2)*30) + " - " + str((i+2)*30 + end_idx_cross))
-                            print("Length bbox vector           : " + str(len(annotations[ped_annt][new_id][i]['bbox'])))
-                            print("Length cross vector          : " + str(len(np.array(tmp_cross[(i+2)*30:(i+2)*30+end_idx_cross]))))
-                            #print("Cross vector                 : " + str(np.array(tmp_cross[(i+2)*30:(i+2)*30+end_idx_cross])))
-                            #print("End index cross              : " + str((i+2)*30 + end_idx_cross))
-                            print("Cross value                  : " + str(annotations[ped_annt][new_id][i]['cross']))
                 
         return annotations, nbr_seq
 
@@ -186,36 +183,20 @@ class JAAD(object):
 
         :return: A database dictionary
         """
-        print("Generating database for jaad\n\n")
-
-        # Generates a list of behavioral xml file names for  videos
-        cache_file = join(self.cache_path, 'jaad_database.pkl')
-        if exists(cache_file) and not self._regen_pkl:
-            with open(cache_file, 'rb') as fid:
-                try:
-                    database = pickle.load(fid)
-                except:
-                    database = pickle.load(fid, encoding='bytes')
-            print('jaad database loaded from {}'.format(cache_file))
-            return database
+        print("Generating database for jaad\n")
 
         video_ids = sorted(self._get_video_ids())
         database = {'split': {'train_ID':[], 'test_ID': []}, 'annotations': {}}
         database_vid_ID = []
         nbr_seq_vid_ID = []
         for vid in video_ids:
-            print('---------------------------------------------------------')
-            print('----------- Getting annotations for %s ----------' % vid)
-            print('---------------------------------------------------------')
+            print('Getting annotations for %s' % vid, end="\r")
             
             vid_annotations, nbr_seq = self._get_annotations(vid)
             if (nbr_seq != 0):
                 database['annotations'][vid] = vid_annotations
                 database_vid_ID.append(vid)
                 nbr_seq_vid_ID.append(nbr_seq)
-                print('\nAnnotations found for %s\n' % vid)
-            else:
-                print('\nNo pedestrian annotations found for %s\n' % vid)   
 
         # Creating testset/trainset
         cumsum = np.cumsum(nbr_seq_vid_ID)/sum(nbr_seq_vid_ID)
@@ -223,12 +204,13 @@ class JAAD(object):
         database['split']['train_ID'] = database_vid_ID[res:]
         database['split']['test_ID'] = database_vid_ID[:res]
 
-        with open(cache_file, 'wb') as fid:
+        path = join(self._jaad_path, 'jaad_database.pkl')
+        with open(path, 'wb') as fid:
             pickle.dump(database, fid, pickle.HIGHEST_PROTOCOL)
-        print('The database is written to {}'.format(cache_file))
+        print('The database is written to {}'.format(path))
 
         return database
 
 if __name__ == "__main__":
-    DS = JAAD(data_path='/home/dimitri/DLAV_ws/JAAD/JAAD', regen_pkl=True)
+    DS = JAAD(data_path=dirname(abspath('__file__')))
     DS.generate_database()
